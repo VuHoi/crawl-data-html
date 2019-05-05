5e530c139920c9d04d13d66cac5eabc0d8af1804
Vu Khac Hoi <toilahoi007@gmail.com>
2019-05-05 18:34:30 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ products_url = 'https://doveprints.com/'
 
 aws_bucket = 'pod-product'
 aws_credentials = {
-    'aws_access_key_id': 'AKIAJ6WYZFQSCS4B4RSA',
-    'aws_secret_access_key': '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'
+    'aws_access_key_id': 'AKIAJ6WYZFQSCS4B4RSAs',
+    'aws_secret_access_key': 'RxttNCAFLpeqqAhsPGX7RsLTBzk9VdheLXSHj2SZs'
 }
